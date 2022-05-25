db71b0be8a111211cc56f20cc79178ed600b837c
Huzeyfe <huzeyfe.abdullahoglu@bilgiedu.net>
2022-05-25 14:26:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arrOfDrivers, name){
+    return arrOfDrivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+
+function fuzzyMatch(arrOfDrivers, name){
+    return arrOfDrivers.filter(driver => driver.toLowerCase().startsWith(name.toLowerCase()))
+}
+
+
+
+function matchName (arrOfDriversObs, name){
+    return arrOfDriversObs.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
+}
